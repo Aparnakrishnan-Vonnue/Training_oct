5d12c795a4f58a17d0fe049302bb2fab0e22ad62
Aparna <aparna.k@vonnue.com>
2022-12-03 15:20:58 +0530

    feat:watched 21mins of javascript tutorial

---
+++ Javascript-tutorial/People-counter-app/index.js
@@ -0,0 +1,36 @@
+//document.getElementById("count-el").innerText=5
+
+//let count = 5/7
+//console.log(count)
+
+// let myage = 24
+// console.log(myage)
+
+//let firstBatch=5
+//let secondBatch=8
+//let count=firstBatch+secondBatch
+//console.log(count)
+
+// let myAge = 24
+// let humanDogRatio = 5
+// let count = myAge*humanDogRatio
+// let myDogAge = count
+// console.log(myDogAge)
+
+//  let count = 5
+//  count=count+1
+//  console.log(count)
+
+//  let bonusPoints = 50
+//  console.log(bonusPoints)
+//  bonusPoints=bonusPoints + 50
+//  console.log(bonusPoints)
+//  bonusPoints= bonusPoints-75
+//  console.log(bonusPoints)
+//  bonusPoints=bonusPoints+45
+//  console.log(bonusPoints)
+
+function increment() {
+    console.log("The button was clicked")  
+}
+
